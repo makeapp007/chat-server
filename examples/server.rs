86f369a866403eb7b17a1c961139e6755c303773
zhouza <zeanzhou@qq.com>
2017-06-14 03:04:48 +0800

    add functions

--- examples/server.rs
+++ examples/server.rs
@@ -1,6 +1,7 @@
 extern crate websocket;
 extern crate serde;
 extern crate serde_json;
+extern crate rand;
 
 #[macro_use]
 extern crate serde_derive;
@@ -9,21 +10,24 @@ use serde_json::Error;
 use serde::ser::{self, Serialize, Serializer};
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::sync::RwLock;
 use std::thread;
 use websocket::OwnedMessage;
 use websocket::sync::Server;
 use std::sync::mpsc::*;
+use rand::{thread_rng, Rng};
 
 #[derive(Debug)]
-enum clientToServerMsg {
+pub enum clientToServerMsg {
 	clientToServerCtrlMsg(clientToServerCtrlMsg),
 	clientToServerTextMsg(clientToServerTextMsg),
 }
 
 #[derive(Debug)]
-enum serverToClientMsg {
+pub enum serverToClientMsg {
 	serverToClientCtrlMsg(serverToClientCtrlMsg),
 	serverToClientTextMsg(serverToClientTextMsg),
 }
@@ -69,9 +73,22 @@ struct User {
 }
 
 #[derive(Debug)]
+struct Group {
+	uid: u32,
+	name: String,
+	user_list: HashSet<u32>,
+}
+
+#[derive(Debug)]
 struct InternalMsg {
 	from: u32,
 	content: OwnedMessage,
+}
+
+#[derive(Debug)]
+enum IDInfo {
+	UserID(u32),
+	GroupID(u32),
 }
 
 fn msgParse(json_string: &str) -> Result<clientToServerMsg, ()> {
@@ -95,7 +112,7 @@ fn msgParse(json_string: &str) -> Result<clientToServerMsg, ()> {
     Err(())
 }
 
-fn msgContruct(structure: serverToClientMsg) -> Result<String, Error> {
+fn msgConstruct(structure: serverToClientMsg) -> Result<String, Error> {
 	let json_string = 
 	match structure {
 		serverToClientMsg::serverToClientTextMsg(m) => serde_json::to_string(&m)?,
@@ -104,27 +121,27 @@ fn msgContruct(structure: serverToClientMsg) -> Result<String, Error> {
 	Ok(json_string)
 }
 
-fn textMsgProcess(m: clientToServerTextMsg, fromID: u32) -> Result<String, Error> {
+fn textMsgProcess(m: &clientToServerTextMsg, from_id: u32) -> Result<String, Error> {
 	match m.toType {
 		0 => {
 			// this message is sent to a user
 			let v: serverToClientTextMsg = serverToClientTextMsg {
 				msgType: 1,
 				fromType: 0,
-				fromID: [fromID, 0],
-				data: m.data,
+				fromID: [from_id, 0],
+				data: m.data.clone(),
 			};
-			msgContruct(serverToClientMsg::serverToClientTextMsg(v))
+			msgConstruct(serverToClientMsg::serverToClientTextMsg(v))
 		},
 		1 => {
 			// this message is sent to a group
 			let v: serverToClientTextMsg = serverToClientTextMsg {
 				msgType: 1,
 				fromType: 1,
-				fromID: [fromID, m.toID], // toID is where the sender lives and receiver stays
-				data: m.data,
+				fromID: [from_id, m.toID], // toID is where the sender lives and receiver stays
+				data: m.data.clone(),
 			};
-			msgContruct(serverToClientMsg::serverToClientTextMsg(v))
+			msgConstruct(serverToClientMsg::serverToClientTextMsg(v))
 		},
 		_ => Err(ser::Error::custom(
 			format!("Invalid toType {0} in clientToServerTextMsg of textMsgProcess().", m.toType)
@@ -132,27 +149,94 @@ fn textMsgProcess(m: clientToServerTextMsg, fromID: u32) -> Result<String, Error
 	}
 }
 
-fn ctrlMsgProcess(m: clientToServerCtrlMsg) -> Result<(), ()> {
+fn ctrlMsgProcess(m: &clientToServerCtrlMsg, from_id: u32, user_table: Arc<Mutex<HashMap<u32, User>>>, group_table: Arc<RwLock<HashMap<u32, Group>>>) -> Result<String, Error> {
+	let mut v: serverToClientCtrlMsg = serverToClientCtrlMsg {
+		msgType: 1,
+		opcode: m.opcode + 1,
+		data: "".to_string(),
+	};
 	match m.opcode {
-		0 => unimplemented!(),
-		2 => unimplemented!(),
-		4 => unimplemented!(),
-		6 => unimplemented!(),
-		8 => unimplemented!(),
-		_ => Err(()),
-	}
+		0 => {
+			// change user nickname
+			user_table.lock().unwrap().get_mut(&from_id).unwrap().name = m.data.clone();
+			v.data = format!("Nickname has been changed to {}", m.data.clone());
+		},
+		2 => {
+			// create new group
+			let mut rng = thread_rng();
+			let mut x: u32 = rng.gen();
+			while group_table.read().unwrap().contains_key(&x) {
+				// TODO: avoid dead loop
+				x = rng.gen();
+			}
 
+			let mut hs = HashSet::new();
+			hs.insert(from_id);
+			let group: Group = Group {
+				uid: 0,
+				name: m.data.clone(),
+				user_list: hs,
+			};
+			group_table.write().unwrap().insert(x, group);
+			v.data = format!("New group is created, group ID = {}", x.to_string());
+		},
+		4 => {
+			// join a group
+			let x = m.data.clone().parse::<u32>();
+			if let Ok(num) = x {
+				match group_table.write().unwrap().get_mut(&num) {
+					Some(expr) => {
+						if expr.user_list.insert(from_id) {
+							v.data = format!("You have joined a new group, group ID = {}", m.data.clone());
+						} else {
+							v.data = format!("You have already joined this group, group ID = {}", m.data.clone());
+						}
+					},
+					None => v.data = format!("Invalid group ID = {}", m.data.clone()),
+				};
+			} else {
+				v.data = format!("Invalid group ID = {}", m.data.clone());
+			}
+		},
+		6 => {
+			// leave a group
+			let x = m.data.clone().parse::<u32>();
+			if let Ok(num) = x {
+				match group_table.write().unwrap().get_mut(&num) {
+					Some(expr) => {
+						if expr.user_list.remove(&from_id) {
+							v.data = format!("You have left this group, group ID = {}", m.data.clone());
+						} else {
+							v.data = format!("You are not in this group, group ID = {}", m.data.clone());
+						}
+					},
+					None => v.data = format!("Invalid group ID = {}", m.data.clone()),
+				};
+			} else {
+				v.data = format!("Invalid group ID = {}", m.data.clone());
+			}
+		},
+		_ => return Err(ser::Error::custom(
+			format!("Invalid opcode {0} in clientToServerCtrlMsg of ctrlMsgProcess().", m.opcode)
+			)),
+	};
+	msgConstruct(serverToClientMsg::serverToClientCtrlMsg(v))
 }
 
 
 fn main() {
 	let server = Server::bind("127.0.0.1:2794").unwrap();
 
-	let h: HashMap<u32, User> = HashMap::new();
-	let user_table_ = Arc::new(Mutex::new(h));
+	let h1: HashMap<u32, User> = HashMap::new();
+	let user_table_ = Arc::new(Mutex::new(h1));
+
+	let h2: HashMap<u32, Group> = HashMap::new();
+	let group_table = Arc::new(RwLock::new(h2));
 
 	let (tx_slaver_, rx_master) = channel(); // send InternalMsg
 	let user_table = user_table_.clone();
+
+	// Master Thread
 	thread::spawn(move || {
 		loop {
 			let internal_msg: InternalMsg = rx_master.recv().unwrap();
@@ -161,18 +245,66 @@ fn main() {
 			let from_uid = internal_msg.from;
 			match message {
 				OwnedMessage::Close(_) => {},
-				OwnedMessage::Text(_) => {
-					// // Broadcasting
-					// let ut = user_table.lock().unwrap();
-					// for user in ut.values() {
-					// 	if user.uid != from_uid {
-					// 		if let OwnedMessage::Text(txt) = message.clone() {
-					// 			let new_message =  OwnedMessage::Text(ut[&from_uid].name.to_string() + ": " + &txt.to_string());
-					// 			user.msg_tx.send(new_message).unwrap();
-					// 		}
-					// 	}
-					// }
-					
+				OwnedMessage::Text(s) => {
+					if let Ok(msg_struct) = msgParse(s.as_ref()) {
+						let id_info: IDInfo;
+						let msg_str_to_send = 
+							match msg_struct {
+								clientToServerMsg::clientToServerTextMsg(m) => {
+									match textMsgProcess(&m, from_uid) {
+										Ok(json_string) => {
+											id_info = 
+												match m.toType {
+													0 => IDInfo::UserID(m.toID),
+													1 => IDInfo::GroupID(m.toID),
+													_ => unimplemented!(),// should never happen
+												};
+											json_string
+										},
+										Err(e) => {
+											println!("{:?}", e);
+											continue;
+										},
+									}
+								},
+								clientToServerMsg::clientToServerCtrlMsg(m) => {
+									match ctrlMsgProcess(&m, from_uid, user_table.clone(), group_table.clone()) {
+										Ok(json_string) => {
+											id_info = IDInfo::UserID(from_uid);
+											json_string
+										},
+										Err(e) => {
+											println!("{:?}", e);
+											continue;
+										},
+									}
+								},
+							};
+						let new_message = OwnedMessage::Text(msg_str_to_send);
+
+						let ut = user_table.lock().unwrap();
+						match id_info {
+							IDInfo::UserID(id) => {
+								ut[&id].msg_tx.send(new_message).unwrap();
+							},
+							IDInfo::GroupID(id) => {
+								if id == 0 {
+									for user in ut.values() {
+										if user.uid != from_uid {
+											user.msg_tx.send(new_message.clone()).unwrap();
+										}
+									}
+								} else {
+									let gt = group_table.read().unwrap();
+									for i in &gt[&id].user_list {
+										ut[i].msg_tx.send(new_message.clone()).unwrap();
+									}
+								};
+							},
+						}
+					} else {
+						println!("Invalid JSON!");
+					}
 				},
 				OwnedMessage::Binary(bin) => {
 					user_table.lock().unwrap().get_mut(&from_uid).unwrap().name = String::from_utf8(bin).unwrap();
@@ -183,6 +315,8 @@ fn main() {
 		};
 
 	});
+
+
 	for request in server.filter_map(Result::ok) {
 		// Spawn a new thread for each connection.
 		let user_table = user_table_.clone();
@@ -237,9 +371,7 @@ fn main() {
 
 			// Receiver Loop
 			for message in receiver.incoming_messages() {
-				print!("{:?}",message );
 				let message = message.unwrap();
-
 				match message {
 					OwnedMessage::Close(_) => {
 						let message = OwnedMessage::Close(None);
@@ -261,6 +393,7 @@ fn main() {
 
 					// if get a msg from client, send it to master
 					_ => {
+						println!("[{}] {:?}", uid, message.clone());
 						tx_slaver.send(InternalMsg{
 											from: uid,
 											content: message,
